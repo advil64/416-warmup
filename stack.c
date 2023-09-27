c9ab27a7a7b0d46d05be71fef0df0b34128022f0
Tajvir Chahal <tajvirchahal87@gmail.com>
2023-09-26 23:18:20 -0400

    Section 1 should be done
    make sure to compile with:
    gcc -m32 stack.c -o stack

--- stack.c
+++ stack.c
@@ -2,32 +2,50 @@
 * Add NetID and names of all project partners
 *
 */
+
+#include <stdio.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <setjmp.h> 
 
-/* Part 1 - Step 1 and 2: Do your tricks here
- * Your goal must be to change the stack frame of caller (main function)
- * such that you get to the line after "r2 = *( (int *) 0 )"
- */
+jmp_buf jump_buffer;
+// jmp_buf type = stores information about the execution context of program
+
+
 void signal_handle(int signalno) {
+    printf("Handling segmentation fault!\n");
 
-    printf("handling segmentation fault!\n");
+    // Set a flag to indicate that the signal has been handled
+    volatile int handled = 1;
 
-    /* Step 2: Handle segfault and change the stack*/
+    // Jump back to the main function - transfer control to main
+    longjmp(jump_buffer, 1);
 }
 
 int main(int argc, char *argv[]) {
-
     int r2 = 0;
 
-    /* Step 1: Register signal handler first*/
+    // Sig handler (SIGSEV - 11)
+    signal(SIGSEGV, signal_handle);
 
+    // if the segmentation is not handled run r2
+    if (setjmp(jump_buffer) == 0) {
+        // Attempt to access memory address 0 (will cause a segmentation fault)
+        r2 = *((int *)0);
+    } else {
+        // This block will be executed after handling the segmentation fault
+        //printf("temp\n");
+        r2 = r2 + 1 * 30;
+    }
 
-    r2 = *( (int *) 0 ); // This will generate segmentation fault
-
-    r2 = r2 + 1 * 30;
-    printf("result after handling seg fault %d!\n", r2);
+    printf("result after handling segfault %d!\n", r2);
 
     return 0;
 }
+
+
+
+
+
+
